0c1f98a1da8f5409f2ffcdf969318ae377d4dead
masterkn <masterkn48@gmail.com>
2019-07-13 00:26:59 +0530

    gh

---
+++ config/key.js
@@ -0,0 +1,12 @@
+module.exports = {
+    mongodb: {
+        dbURI: 'mongodb+srv://User:QazXsw@123@cluster0-0rupj.mongodb.net/test?retryWrites=true&w=majority'
+    },
+    session: {
+        cookieKey: 'f_ckU#213'
+    },
+    google:{
+        clientID:'679977054902-4lq6usgsc77sq0rciutji9b6oubrk0j4.apps.googleusercontent.com',
+        clientSecret:'ZeimujiCkLsb2BAt9smdTWQ_'
+    }
+};
